72691cb433f873caf6b6072b2e859b83ca312420
Silas Berger <silas.berger@outlook.com>
2023-06-08 17:13:16 +0200

    Add seed script.

---
+++ db/insert_demo_data.sql
@@ -0,0 +1,4 @@
+insert into video(source_url) values ('https://vm.tiktok.com/ZGJ5GmVJX/');
+insert into video(source_url) values ('https://vm.tiktok.com/ZGJ5Gtwvs/');
+insert into video(source_url) values ('https://vm.tiktok.com/ZMYXA9gxK/');
+insert into video(source_url) values ('https://vm.tiktok.com/ZGJ5cauXS/');
