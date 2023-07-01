c85be20566a3e185f9e41fe1dd33aeac81037a49
Silas Berger <silas.berger@outlook.com>
2023-07-01 19:41:39 +0200

    Clean up configs.

--- dev/insert_demo_data.sql
+++ dev/insert_demo_data.sql
@@ -1,5 +1,5 @@
-insert into source_url (url) values ('https://vm.tiktok.com/ZGJ5GmVJX/');
-insert into source_url (url) values ('https://vm.tiktok.com/ZGJ5Gtwvs/');
-insert into source_url (url) values ('https://vm.tiktok.com/ZMYXA9gxK/');
-insert into source_url (url) values ('https://vm.tiktok.com/this-should-fail/');
-insert into source_url (url) values ('https://vm.tiktok.com/ZGJ5cauXS/');
+insert into source_url (url) values ('https://www.tiktok.com/@chloethecoder/video/7220006434757414146');
+insert into source_url (url) values ('https://www.tiktok.com/@dev.ui/video/7221509042089381121');
+insert into source_url (url) values ('https://www.tiktok.com/@konstantinpaulus/video/7222698934135770374');
+insert into source_url (url) values ('https://www.tiktok.com/@.lachief/video/thisshouldfail');
+insert into source_url (url) values ('https://www.tiktok.com/@jhankarmahbub/video/7202782477717343531');
