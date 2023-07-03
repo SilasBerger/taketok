3026859c91136826775a31cb80f39818229d08d1
Silas Berger <silas.berger@outlook.com>
2023-07-03 11:33:44 +0200

    Optimize imports.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::ops::Mul;
-use std::time::Duration;
-
 use diesel::{Connection, QueryDsl, RunQueryDsl, SelectableHelper, SqliteConnection};
 
 use crate::core_api_client::{CoreApiClient};
